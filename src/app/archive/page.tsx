7d74873bdb289c47d15b5fa8828d200061462eff
CorinaNC <corina0615@gmail.com>
2025-05-24 11:58:39 -0500

    Junior year heh

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -4,6 +4,16 @@ export default function Archive() {
     return (
         <Container alignContent="center">
             <Heading textAlign="left" fontStyle="bold" fontFamily="Courier">
+                2025
+            </Heading>
+            <Divider mb={5} />
+            <HStack alignItems="center" mb={3}>
+                <List textDecoration="underline">
+                    <Link href="archive/juniorYearInternshipHunt">
+                    {'>'} Junior Year </Link>
+                </List>
+            </HStack>
+            <Heading textAlign="left" fontStyle="bold" fontFamily="Courier">
                 2024
             </Heading>
             <Divider mb={5} />
